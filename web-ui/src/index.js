e144b8c4482850011b9ffd773bc35f61018aed39
Alex Kehayias <alex@mosey.so>
2024-09-15 14:04:59 -0400

    Add search results to UI

--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -1,9 +1,11 @@
 const searchInput = document.getElementById("search");
+const resultList = document.getElementById("results");
+const emptyState = document.getElementById("empty-state");
+
 searchInput.addEventListener("input", async (e) => {
   const val = e.target.value;
 
   if (val) {
-    console.log("Searching:", val);
     try {
       const headers = new Headers();
       headers.append("Content-Type", "application/json");
@@ -19,8 +21,37 @@ searchInput.addEventListener("input", async (e) => {
         throw new Error(`Error fetching: ${response.status}`);
       }
 
-      const json = await response.json();
-      console.log(json);
+      const data = await response.json();
+
+      if (data.results.length > 0) {
+        emptyState.classList.add("hidden");
+        resultList.classList.remove("hidden");
+
+        const hits = data.results.map((r) => {
+          const hit = document.createElement("li");
+          hit.classList.add(...[
+            "group",
+            "flex",
+            "cursor-default",
+            "select-none",
+            "items-center",
+            "rounded-md",
+            "px-3",
+            "py-2",
+          ]);
+          hit.innerText = r.title;
+          hit.addEventListener("click", (clickEvent) => {
+            // TODO: Handle selected results
+            console.log(`Clicked result with id ${r.id}`);
+          })
+          return hit;
+        })
+
+        resultList.replaceChildren(...hits);
+      } else {
+        resultList.classList.add("hidden");
+        emptyState.classList.remove("hidden");
+      }
     } catch (error) {
       console.error("Server error", error.message);
     }

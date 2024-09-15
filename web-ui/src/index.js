3e4d0835e2e346fe28c71cf20660b0505cccb6b2
Alex Kehayias <alex@mosey.so>
2024-09-15 17:07:08 -0400

    Add routes for setting latest hit and retrieving it

--- web-ui/src/index.js
+++ web-ui/src/index.js
@@ -2,58 +2,74 @@ const searchInput = document.getElementById("search");
 const resultList = document.getElementById("results");
 const emptyState = document.getElementById("empty-state");
 
-searchInput.addEventListener("input", async (e) => {
-  const val = e.target.value;
+(async function() {
+  searchInput.addEventListener("input", async (e) => {
+    const val = e.target.value;
 
-  if (val) {
-    try {
-      const headers = new Headers();
-      headers.append("Content-Type", "application/json");
+    if (val) {
+      try {
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json");
 
-      const response = await fetch(
-        `http://localhost:2222/notes/search?query=${val.trim()}`,
-        {
-          method: "GET",
-          headers,
+        const response = await fetch(
+          `/notes/search?query=${val.trim()}`,
+          {
+            method: "GET",
+            headers,
+          }
+        );
+        if (!response.ok) {
+          throw new Error(`Error fetching: ${response.status}`);
         }
-      );
-      if (!response.ok) {
-        throw new Error(`Error fetching: ${response.status}`);
-      }
 
-      const data = await response.json();
+        const data = await response.json();
 
-      if (data.results.length > 0) {
-        emptyState.classList.add("hidden");
-        resultList.classList.remove("hidden");
+        if (data.results.length > 0) {
+          emptyState.classList.add("hidden");
+          resultList.classList.remove("hidden");
 
-        const hits = data.results.map((r) => {
-          const hit = document.createElement("li");
-          hit.classList.add(...[
-            "group",
-            "flex",
-            "cursor-default",
-            "select-none",
-            "items-center",
-            "rounded-md",
-            "px-3",
-            "py-2",
-          ]);
-          hit.innerText = r.title;
-          hit.addEventListener("click", (clickEvent) => {
-            // TODO: Handle selected results
-            console.log(`Clicked result with id ${r.id}`);
+          const hits = data.results.map((r) => {
+            const hit = document.createElement("li");
+            hit.classList.add(...[
+              "group",
+              "flex",
+              "cursor-default",
+              "select-none",
+              "items-center",
+              "rounded-md",
+              "px-3",
+              "py-2",
+            ]);
+            hit.innerText = r.title;
+            hit.addEventListener("click", async (clickEvent) => {
+              console.log(`Clicked result with id ${r.id}`);
+              const resp = await fetch(
+                `/notes/search/latest`,
+                {
+                  method: "POST",
+                  body: JSON.stringify({id: r.id[0]}),
+                  headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                  },
+                }
+              );
+              if (!resp.ok) {
+                throw new Error(`Error updating latest hit: ${response.status}`);
+              } else {
+                console.log(`Updated latest hit to ${r.id}`)
+              }
+            })
+            return hit;
           })
-          return hit;
-        })
-
-        resultList.replaceChildren(...hits);
-      } else {
-        resultList.classList.add("hidden");
-        emptyState.classList.remove("hidden");
+          resultList.replaceChildren(...hits);
+        } else {
+          resultList.classList.add("hidden");
+          emptyState.classList.remove("hidden");
+        }
+      } catch (error) {
+        console.error("Server error", error.message);
       }
-    } catch (error) {
-      console.error("Server error", error.message);
     }
-  }
-});
+  });
+})();

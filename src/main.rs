e636e92d10cf386cc7fa23faa0003824ce689c48
Alex Kehayias <alex@mosey.so>
2024-09-28 21:04:14 -0400

    Extract out the indexing schema

--- src/main.rs
+++ src/main.rs
@@ -27,15 +27,9 @@ use serde::Deserialize;
 use serde_json::{Value, json};
 use orgize::ParseConfig;
 
-fn note_schema() -> Schema {
-    let mut schema_builder = Schema::builder();
-    schema_builder.add_text_field("id", TEXT | STORED);
-    schema_builder.add_text_field("title", TEXT | STORED);
-    schema_builder.add_text_field("tags", TEXT | STORED);
-    schema_builder.add_text_field("body", TEXT);
-    schema_builder.add_text_field("file_name", TEXT);
-    schema_builder.build()
-}
+mod schema;
+use schema::note_schema;
+
 
 // There is no such thing as updates in tantivy so this function will
 // produce duplicates if called repeatedly
---
+++ src/schema.rs
@@ -0,0 +1,11 @@
+use tantivy::schema::*;
+
+pub fn note_schema() -> Schema {
+    let mut schema_builder = Schema::builder();
+    schema_builder.add_text_field("id", TEXT | STORED);
+    schema_builder.add_text_field("title", TEXT | STORED);
+    schema_builder.add_text_field("tags", TEXT | STORED);
+    schema_builder.add_text_field("body", TEXT);
+    schema_builder.add_text_field("file_name", TEXT);
+    schema_builder.build()
+}

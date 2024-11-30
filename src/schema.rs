6a4fa0a9f54b3aa0b8cea920d45f750c643a5bcf
Alex Kehayias <alex@mosey.so>
2024-11-30 10:20:07 -0500

    Add tasks to search index

--- src/schema.rs
+++ src/schema.rs
@@ -3,8 +3,10 @@ use tantivy::schema::*;
 pub fn note_schema() -> Schema {
     let mut schema_builder = Schema::builder();
     schema_builder.add_text_field("id", TEXT | STORED);
+    schema_builder.add_text_field("type", TEXT | STORED);
     schema_builder.add_text_field("title", TEXT | STORED);
     schema_builder.add_text_field("tags", TEXT | STORED);
+    schema_builder.add_text_field("status", TEXT | STORED);
     schema_builder.add_text_field("body", TEXT);
     schema_builder.add_text_field("file_name", TEXT | STORED);
     schema_builder.build()

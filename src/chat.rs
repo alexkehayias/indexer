ea6ad4295c4e2a98c1a58a095345bc4ebd5b5088
Alex Kehayias <alex@mosey.so>
2025-02-08 13:05:55 -0500

    Extract tool calls into their own function

--- src/chat.rs
+++ src/chat.rs
@@ -1,5 +1,43 @@
-use serde_json::json;
+use serde_json::{json, Value};
 use crate::openai::{Message, Role, ToolCall, FunctionCall, FunctionCallFn, completion};
+
+
+fn handle_tool_calls(history: &mut Vec<Message>, tools: &Vec<Box<dyn ToolCall>>, tool_calls: &Vec<Value>) {
+    // Handle each tool call
+    for tool_call in tool_calls {
+        let tool_call_id = &tool_call["id"].as_str().unwrap();
+        let tool_call_function = &tool_call["function"];
+        let tool_call_args =
+            tool_call_function["arguments"].as_str().unwrap();
+        let tool_call_name = tool_call_function["name"].as_str().unwrap();
+
+        // Call the tool and get the next completion from the result
+        let tool_call_result = tools.iter()
+            .find(|i| *i.function_name() == *tool_call_name)
+            .unwrap_or_else(|| {
+                panic!(
+                    "Received tool call that doesn't exist: {}",
+                    tool_call_name
+                )
+            })
+            .call(tool_call_args);
+        let tool_call_requests = vec![FunctionCall {
+            function: FunctionCallFn {
+                arguments: tool_call_args.to_string(),
+                name: tool_call_name.to_string(),
+            },
+            id: tool_call_id.to_string(),
+            r#type: String::from("function"),
+        }];
+        history.push(Message::new_tool_call_request(
+            tool_call_requests,
+        ));
+        history.push(Message::new_tool_call_response(
+            &tool_call_result,
+            tool_call_id,
+        ));
+    };
+}
 
 pub async fn chat(history: &mut Vec<Message>, tools: &Option<Vec<Box<dyn ToolCall>>>) {
     let resp = completion(history, tools)
@@ -10,45 +48,14 @@ pub async fn chat(history: &mut Vec<Message>, tools: &Option<Vec<Box<dyn ToolCal
     let tool_calls = resp["choices"][0]["message"]["tool_calls"].as_array();
     if let Some(tool_calls) = tool_calls {
         if let Some(ref tools_ref) = tools {
-            // Handle each tool call
-            for tool_call in tool_calls {
-                let tool_call_id = &tool_call["id"].as_str().unwrap();
-                let tool_call_function = &tool_call["function"];
-                let tool_call_args =
-                    tool_call_function["arguments"].as_str().unwrap();
-                let tool_call_name = tool_call_function["name"].as_str().unwrap();
+            handle_tool_calls(history, tools_ref, tool_calls);
 
-                // Call the tool and get the next completion from the result
-                let tool_call_result = tools_ref
-                    .iter()
-                    .find(|i| *i.function_name() == *tool_call_name)
-                    .unwrap_or_else(|| {
-                        panic!(
-                            "Received tool call that doesn't exist: {}",
-                            tool_call_name
-                        )
-                    })
-                    .call(tool_call_args);
-                let tool_call_requests = vec![FunctionCall {
-                    function: FunctionCallFn {
-                        arguments: tool_call_args.to_string(),
-                        name: tool_call_name.to_string(),
-                    },
-                    id: tool_call_id.to_string(),
-                    r#type: String::from("function"),
-                }];
-                history.push(Message::new_tool_call_request(
-                    tool_call_requests,
-                ));
-                history.push(Message::new_tool_call_response(
-                    &tool_call_result,
-                    tool_call_id,
-                ));
-            }
+            // Provide the results of the tool calls back to the chat
             let resp = completion(history, tools)
                 .await
                 .expect("OpenAI API call failed");
-            // TODO: Is it possible the response is another tool call?
+
+            // If there wasn't a response, something went wrong
             let msg = resp["choices"][0]["message"]["content"]
                 .as_str()
                 .unwrap_or_else(|| {

742bb6a8b59ba0e24e1d3817918ee63f75929771
Alex Kehayias <alex@mosey.so>
2025-10-04 10:52:31 -0700

    Set a push notification tag when notifying about re-indexing

--- src/server.rs
+++ src/server.rs
@@ -369,7 +369,7 @@ async fn send_notification(
     };
 
     let payload =
-        PushNotificationPayload::new("Notification", &payload.message, None, None, None);
+        PushNotificationPayload::new("Notification", &payload.message, None, None, Some("indexer_updated"));
     broadcast_push_notification(subscriptions, vapid_key_path, payload).await;
 
     Ok(Json(json!({ "success": true })))

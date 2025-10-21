58074b223a3e6d7185c13f5f2d0abfaf0935a8de
Alex Kehayias <alex.kehayias@gmail.com>
2025-10-20 19:22:02 -0700

    Fix diffing files for re-indexing

--- src/git.rs
+++ src/git.rs
@@ -1,6 +1,6 @@
 use std::process::Command;
 
-// Clone a repo if it doesn't already exist
+/// Clone a repo if it doesn't already exist
 pub fn maybe_clone_repo(deploy_key_path: &str, url: &str, storage_path: &str) {
     let git_clone = Command::new("sh")
         .arg("-c")
@@ -16,30 +16,33 @@ pub fn maybe_clone_repo(deploy_key_path: &str, url: &str, storage_path: &str) {
     println!("stdout: {}\nstderr: {}", stdout, stderr);
 }
 
-// Pull and reset to origin main branch
+/// Pull and reset to origin main branch
 pub fn maybe_pull_and_reset_repo(deploy_key_path: &str, path: &str) {
     let git_clone = Command::new("sh")
         .arg("-c")
         .arg(format!("cd {} && GIT_SSH_COMMAND='ssh -i {} -o IdentitiesOnly=yes' git fetch origin && git reset --hard origin/main", path, deploy_key_path))
         .output()
-        .expect("failed to execute process");
+        .expect("Failed to execute process");
 
     let stdout = std::str::from_utf8(&git_clone.stdout).expect("Failed to parse stdout");
     let stderr = std::str::from_utf8(&git_clone.stderr).expect("Failed to parse stderr");
     tracing::debug!("stdout: {}\nstderr: {}", stdout, stderr);
 }
 
-// Return a list of files that have changed between local and origin
-pub fn diff_files(deploy_key_path: &str, path: &str) -> Vec<String> {
+/// Return a list of files that have changed between the last two
+/// commits.  Run `maybe_pull_and_reset_repo` before hand if you want
+/// to get a list of files that changed on origin.
+pub fn diff_last_commit_files(deploy_key_path: &str, path: &str) -> Vec<String> {
+    // Run git diff
     let command = Command::new("sh")
         .arg("-c")
         .arg(format!(
-            "cd {} && GIT_SSH_COMMAND='ssh -i {} -o IdentitiesOnly=yes' git diff --name-only HEAD..origin/main | grep -vF -e \"$(git ls-files)\"",
+            "cd {} && GIT_SSH_COMMAND='ssh -i {} -o IdentitiesOnly=yes' git --no-pager diff --name-only HEAD^ HEAD",
             path,
             deploy_key_path
         ))
         .output()
-        .expect("failed to execute process");
+        .expect("Failed to execute process");
 
     let stdout = std::str::from_utf8(&command.stdout).expect("Failed to parse stdout");
     let stderr = std::str::from_utf8(&command.stderr).expect("Failed to parse stderr");

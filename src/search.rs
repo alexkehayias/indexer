1386dec03061559c8f758cf43e7255a65484d993
Alex Kehayias <alex.kehayias@gmail.com>
2025-10-20 19:22:01 -0700

    Format code with rustfmt

--- src/search.rs
+++ src/search.rs
@@ -9,25 +9,31 @@ use super::schema::note_schema;
 pub fn search_notes(query: &String) -> Vec<NamedFieldDocument> {
     let schema = note_schema();
     let index_path = tantivy::directory::MmapDirectory::open("./.index").expect("Index not found");
-    let idx = Index::open_or_create(index_path, schema.clone()).expect("Unable to open or create index");
+    let idx =
+        Index::open_or_create(index_path, schema.clone()).expect("Unable to open or create index");
     let title = schema.get_field("title").unwrap();
     let body = schema.get_field("body").unwrap();
 
     let reader = idx
         .reader_builder()
         .reload_policy(ReloadPolicy::OnCommitWithDelay)
-        .try_into().expect("Reader failed to load");
+        .try_into()
+        .expect("Reader failed to load");
 
     let searcher = reader.searcher();
     let query_parser = QueryParser::for_index(&idx, vec![title, body]);
 
-    let query = query_parser.parse_query(query).expect("Failed to parse query");
+    let query = query_parser
+        .parse_query(query)
+        .expect("Failed to parse query");
 
-    searcher.search(&query, &TopDocs::with_limit(10))
+    searcher
+        .search(&query, &TopDocs::with_limit(10))
         .expect("Search failed")
         .iter()
-        .map(|(_score, doc_addr) | {
-            searcher.doc::<TantivyDocument>(*doc_addr)
+        .map(|(_score, doc_addr)| {
+            searcher
+                .doc::<TantivyDocument>(*doc_addr)
                 .expect("Doc not found")
                 .to_named_doc(&schema)
         })

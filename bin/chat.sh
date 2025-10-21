1f12e14a4839079b5508d42c2b856750a67c9a1e
Alex Kehayias <alex.kehayias@gmail.com>
2025-10-20 19:22:05 -0700

    Update chat script so it can be executed from another dir

--- bin/chat.sh
+++ bin/chat.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-cargo run chat
+BIN_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPT_PATH="$(dirname $BIN_PATH)"
+
+/usr/bin/env -S /bin/sh -c "cd $SCRIPT_PATH && cargo run chat" "$@"

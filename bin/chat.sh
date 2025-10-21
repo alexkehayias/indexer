dc68c9437174e6d837c74526e2cd00366dfe1de9
Alex Kehayias <alex.kehayias@gmail.com>
2025-10-20 19:23:29 -0700

    Update dev server scripts to auto reload

--- bin/chat.sh
+++ bin/chat.sh
@@ -3,4 +3,4 @@
 BIN_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 SCRIPT_PATH="$(dirname $BIN_PATH)"
 
-/usr/bin/env -S /bin/sh -c "cd $SCRIPT_PATH && cargo run chat" "$@"
+/usr/bin/env -S /bin/zsh -c "cd $SCRIPT_PATH && cargo run chat" "$@"

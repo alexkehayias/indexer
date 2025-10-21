a04e364e9aa186b43572693ba807be403608ef69
Alex Kehayias <alex.kehayias@gmail.com>
2025-10-20 19:21:59 -0700

    Add server route that searches the index

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ use tantivy::schema::*;
 use tantivy::{doc, Index, IndexWriter, ReloadPolicy};
 
 use axum::{
-    body::Body,
     routing::get,
     response::Json,
     Router,
@@ -133,9 +132,40 @@ struct Args {
 
 // Fulltext search of all notes
 async fn search(Query(params): Query<HashMap<String, String>>) -> Json<Value> {
+
+    let schema = note_schema();
+    let index_path = tantivy::directory::MmapDirectory::open("./.index").expect("Index not found");
+    let idx = Index::open_or_create(index_path, schema.clone()).expect("Unable to open or create index");
+    let title = schema.get_field("title").unwrap();
+    let body = schema.get_field("body").unwrap();
+
+    let reader = idx
+        .reader_builder()
+        .reload_policy(ReloadPolicy::OnCommitWithDelay)
+        .try_into().expect("Reader failed to load");
+
+    let searcher = reader.searcher();
+    let query_parser = QueryParser::for_index(&idx, vec![title, body]);
+
+    let results: Vec<NamedFieldDocument> = if let Some(query) = params.get("query") {
+        let query = query_parser.parse_query(query).expect("Failed to parse query");
+
+        searcher.search(&query, &TopDocs::with_limit(10))
+            .expect("Search failed")
+            .iter()
+            .map(|(_score, doc_addr) | {
+                searcher.doc::<TantivyDocument>(*doc_addr)
+                    .expect("Doc not found")
+                    .to_named_doc(&schema)
+            })
+            .collect()
+    } else {
+        Vec::new()
+    };
+
     let resp = json!({
         "query": params.get("query"),
-        "results": vec!(""),
+        "results": results,
     });
     Json(resp)
 }

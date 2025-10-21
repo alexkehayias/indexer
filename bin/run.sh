73165eed3ef18109dc98392318c91679a262d298
Alex Kehayias <alex.kehayias@gmail.com>
2025-10-20 19:22:04 -0700

    Also run tests via watcher

--- bin/run.sh
+++ bin/run.sh
@@ -1,1 +1,1 @@
-cd ./web-ui/ && npx tailwindcss -i ./src/input.css -o ./src/output.css -m && cd ../ && cargo run -- serve --host localhost --port 2222
+cd ./web-ui/ && npx tailwindcss -i ./src/input.css -o ./src/output.css -m && cd ../ && cargo test && cargo run -- serve --host localhost --port 2222
